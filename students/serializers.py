f5d2c7f91c33de14ce50176d7bcc6fa996bec591
Rustam T <sacred-foundry@yandex.ru>
2023-07-14 16:00:38 +0700

    update2

--- students/serializers.py
+++ students/serializers.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from rest_framework import serializers
 
 from students.models import Course
@@ -8,3 +9,9 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def validate_students(self, data):
+        # print(Course.objects.get(id=1).students.count())
+            if Course.objects.get(name='test1').students.count() > 10:
+                raise ValidationError('Превышено количество студентов, макс = 20')
+            return data
--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -11,19 +11,26 @@ def client():
 def course_factory():
     def factory(*args, **kwargs):
         return baker.make(Course, *args, **kwargs)
-    
     return factory
+
+@pytest.fixture
+def students_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+    return factory
+
 
 @pytest.mark.django_db
 def test_get_first_course(client, course_factory):
     #Arrange
     courses = course_factory(_quantity=1)
     #Act
-    response = client.get('/api/v1/courses/')
+    response = client.get('/api/v1/courses/1/')
     data = response.json()
     #Assert
     assert response.status_code == 200
-    assert courses[0].id == data[0]['id']
+    assert courses[0].id == data['id']
+    assert courses[0].name == data['name']
     
 @pytest.mark.django_db
 def test_get_all_courses(client, course_factory):
@@ -42,11 +49,11 @@ def test_filter_course_by_id(client, course_factory):
     #Arrange
     courses = course_factory(_quantity=10)
     #Act
-    response = client.get(f'/api/v1/courses/{courses[0].id}/')
+    response = client.get('/api/v1/courses/', {'id':courses[0].id})
     data = response.json()
     #Assert
     assert response.status_code == 200
-    assert data['id'] == courses[0].id
+    assert data[0]['id'] == courses[0].id
     
 @pytest.mark.django_db
 def test_filter_course_name(client, course_factory):
@@ -77,7 +84,7 @@ def test_update_course(client, course_factory):
     response = client.patch(f'/api/v1/courses/{courses[0].id}/', data ={'name':'test_update'})
     #Assert
     assert response.status_code == 200
-    assert Course.objects.filter(name='test_update').exists() is True
+    assert response.data['name'] == 'test_update'
     
 @pytest.mark.django_db
 def test_delete_course(client, course_factory):
@@ -91,8 +98,16 @@ def test_delete_course(client, course_factory):
     assert Course.objects.count() == count - 1
     
 @pytest.mark.django_db
-def test_max_stunedts_at_course(max_student=20, test_count=10):
+def test_max_stunedts_at_course(client, students_factory):
     #Arrange
-    if Course.objects.count() + test_count > max_student:
-        raise AssertionError(f'Превышено допустимое количество студентов на курсе, максимум - {max_student}')
-    assert Course.objects.count() + test_count < max_student
+    students_test = students_factory(_quantity=21)
+    x = Course.objects.create(name='test1')
+    x.save()
+    # y = Student.objects.create(name='123')
+    # x.students.add(y)
+    for s in students_test:
+        # Course.objects.update_or_create(name='test1', students = s)
+        x.students.add(s)
+        x.save()
+    # assert Course.objects.get(name='test1').students.count() < 20
+    
